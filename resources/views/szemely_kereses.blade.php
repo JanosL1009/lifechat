e2a66b9a60c0f3a5a5181d385e9218890a046058
Lakatos Balázs <mrdr0nex@gmail.com>
2024-10-30 13:19:03 +0100

    közepre helyezve a keresett szemelyek adatai

--- resources/views/szemely_kereses.blade.php
+++ resources/views/szemely_kereses.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.app')
 @section('content')
+<style>
+  .searchuserimg{
+    text-align:center;
+  }
+</style>
 <div class="container">
   <div class="col-md-12">
     <form action="{{ route('admin.szemely.kereses') }}" method="GET" class="mb-4">
@@ -13,21 +18,21 @@
         <table class="table table-bordered">
             <thead>
                 <tr>
-                    <th>Felhasználónév</th>
-                    <th>Email</th>
-                    <th>Profilkép</th>
-                    <th>Műveletek</th>
+                    <th style="text-align:center;">Felhasználónév</th>
+                    <th style="text-align:center;">Email</th>
+                    <th style="text-align:center;">Profilkép</th>
+                    <th style="text-align:center;">Műveletek</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach($users as $user)
                     <tr>
-                        <td>{{ $user->username }}</td>
-                        <td>{{ $user->email }}</td>
-                        <td>
-                            <img src="{{ asset($user->profilepicture ? 'profilepicture/' . $user->profilepicture : 'profilepicture/default.jpg') }}" alt="Profile Picture" style="width: 50px; height: 50px;">
+                        <td style="text-align:center;">{{ $user->username }}</td>
+                        <td style="text-align:center;">{{ $user->email }}</td>
+                        <td class="searchuserimg">
+                            <img src="{{ asset($user->profilepicture ? 'profilepicture/' . $user->profilepicture : 'profilepicture/default.jpg') }}" alt="Profile Picture" style="width: 50px; height: 50px;" class="searchuserimg">
                         </td>
-                        <td>
+                        <td style="text-align:center;">
                           <a href="{{ route('admin.szemely.szerkesztes', $user->id) }}" class="btn btn-sm btn-secondary">Szerkesztés</a>
                         </td>
                     </tr>
