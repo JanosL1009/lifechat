5a790bbbd8d136a8e3dadcfc2f1da93e02e4393c
Lakatos Balázs <mrdr0nex@gmail.com>
2024-11-04 17:34:50 +0100

    jav.

--- app/Models/RoomOperators.php
+++ app/Models/RoomOperators.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class RoomOperators extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'room_id',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
